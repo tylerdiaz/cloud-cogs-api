eeaeeab0ef84c149a28e50d128ff050e1ef8ccc4
Tyler J. Diaz <me@tylerdiaz.com>
2016-10-04 21:58:30 +0300

    Add password encryption to User.changeset

--- web/models/user.ex
+++ web/models/user.ex
@@ -5,6 +5,7 @@ defmodule CloudCogs.User do
     field :email, :string
     field :username, :string
     field :encrypted_password, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -14,7 +15,20 @@ defmodule CloudCogs.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :username, :encrypted_password])
-    |> validate_required([:email, :username, :encrypted_password])
+    |> cast(params, [:email, :username, :password])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_required([:email, :username, :password])
+    |> unique_constraint(:email)
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        current_changeset
+    end
   end
 end
