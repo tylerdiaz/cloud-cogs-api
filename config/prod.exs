cfed445c745f1e3cff0dc9242cd82f8b41817e32
Tyler J. Diaz <me@tylerdiaz.com>
2016-10-09 23:22:54 +0300

    prod config typo

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :cloud_cogs, CloudCogs.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "powerful-citadel-33409.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :cloud_cogs, CloudCogs.Repo,
