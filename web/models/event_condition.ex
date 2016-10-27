919514992f1a6d136671b7ec8f9bef3cb5f7658a
Tyler J. Diaz <me@tylerdiaz.com>
2016-10-27 08:48:16 +0300

    Make effect/condition payloads into structs

--- web/models/event_condition.ex
+++ web/models/event_condition.ex
@@ -4,7 +4,7 @@ defmodule CloudCogs.EventCondition do
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "event_conditions" do
     field :key, :string
-    field :payload, :map
+    embeds_one :payload, CloudCogs.ConditionPayload
     belongs_to :event, CloudCogs.Event, type: Ecto.UUID
 
     timestamps()
@@ -15,7 +15,25 @@ defmodule CloudCogs.EventCondition do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:key, :payload])
-    |> validate_required([:key, :payload])
+    |> cast(params, [:key])
+    |> cast_embed(:payload, required: true)
+    |> validate_required([:key])
+  end
+end
+
+defmodule CloudCogs.ConditionPayload do
+  use Ecto.Model
+
+  embedded_schema do
+    field :quantity, :integer
+  end
+
+  def import_from_map(payload_map) do
+    %CloudCogs.ConditionPayload{}
+    |> Map.merge(string_keys_to_atoms(payload_map))
+  end
+
+  def string_keys_to_atoms(map) do
+    for {key, val} <- map, into: %{}, do: {String.to_atom(key), val}
   end
 end
--- web/models/event_effect.ex
+++ web/models/event_effect.ex
@@ -4,7 +4,7 @@ defmodule CloudCogs.EventEffect do
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "event_effects" do
     field :key, :string
-    field :payload, :map
+    embeds_one :payload, CloudCogs.EffectPayload
     belongs_to :event, CloudCogs.Event, type: Ecto.UUID
 
     timestamps()
@@ -15,7 +15,28 @@ defmodule CloudCogs.EventEffect do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:key, :payload])
-    |> validate_required([:key, :payload])
+    |> cast(params, [:key])
+    |> cast_embed(:payload, required: true)
+    |> validate_required([:key])
+  end
+end
+
+defmodule CloudCogs.EffectPayload do
+  use Ecto.Schema
+
+  # how can I make these optional on db population? all rows now have like :quantity NULL
+  embedded_schema do
+    field :quantity, :integer
+    field :narrative, {:array, :string}
+    field :model_id, Ecto.UUID
+  end
+
+  def import_from_map(payload_map) do
+    %CloudCogs.EffectPayload{}
+    |> Map.merge(string_keys_to_atoms(payload_map))
+  end
+
+  def string_keys_to_atoms(map) do
+    for {key, val} <- map, into: %{}, do: {String.to_atom(key), val}
   end
 end
