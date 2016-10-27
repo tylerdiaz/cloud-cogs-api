e27f434628645fb749005fac811175b8f204eb42
Tyler J. Diaz <me@tylerdiaz.com>
2016-10-27 08:46:28 +0300

    Drop helper functions on event model
    Thought I would need these tree fns. No immediate use for them.

--- web/models/event.ex
+++ web/models/event.ex
@@ -28,35 +28,4 @@ defmodule CloudCogs.Event do
     |> validate_required([:name, :cause_type ])
   end
 
-  @doc """
-    Recursively loads parents into the given struct until it hits nil
-  """
-  def load_parents(parent) do
-    load_parents(parent, 10)
-  end
-
-  def load_parents(_, limit) when limit < 0, do: raise "Recursion limit reached"
-
-  def load_parents(%Event{parent: nil} = parent, _), do: parent
-
-  def load_parents(%Event{parent: %Ecto.Association.NotLoaded{}} = parent, limit) do
-    parent = parent |> Repo.preload(:parent)
-    Map.update!(parent, :parent, &Event.load_parents(&1, limit - 1))
-  end
-
-  def load_parents(nil, _), do: nil
-
-  @doc """
-    Recursively loads children into the given struct until it hits []
-  """
-  def load_children(model), do: load_children(model, 10)
-
-  def load_children(_, limit) when limit < 0, do: raise "Recursion limit reached"
-
-  def load_children(%Event{children: %Ecto.Association.NotLoaded{}} = model, limit) do
-    model = model |> Repo.preload(:children) # maybe include a custom query here to preserve some order
-    Map.update!(model, :children, fn(list) ->
-      Enum.map(list, &Event.load_children(&1, limit - 1))
-    end)
-  end
 end
